b912d18d4013b84bad71ac5231215b5f3df78535
nickevin <aopfilter@163.com>
2025-05-19 09:23:32 +0800

    refactor

--- src/main/java/com/demo/chat/controller/ChatController.java
+++ src/main/java/com/demo/chat/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.demo.chat.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -18,21 +21,26 @@ public class ChatController {
 
     private final ExecutorService executorService = Executors.newCachedThreadPool();
 
-    @PostMapping("/chat/sse")
-    public SseEmitter sse(@RequestBody ChatRequest request) {
-        log.error("{}", request);
+    @PostMapping("/chat/streaming")
+    public SseEmitter streaming(@RequestBody ChatRequest request) {
+        log.debug("{}", request);
 
         SseEmitter emitter = new SseEmitter();
         executorService.execute(() -> {
             try {
+                emitter.send("{\"delta\":\"<think>\",\"status\":\"delta\"}");
+                emitter.send("{\"delta\":\"你\",\"status\":\"delta\"}");
+                emitter.send("{\"delta\":\"好\",\"status\":\"delta\"}");
+                emitter.send("{\"delta\":\"！\",\"status\":\"delta\"}");
+                emitter.send("{\"delta\":\"</think>\",\"status\":\"delta\"}");
                 emitter.send("{\"delta\":\"hello\",\"status\":\"delta\"}");
-                TimeUnit.MILLISECONDS.sleep(250);
+                TimeUnit.MILLISECONDS.sleep(50);
                 emitter.send("{\"delta\":\" world\",\"status\":\"delta\"}");
-                TimeUnit.MILLISECONDS.sleep(250);
+                TimeUnit.MILLISECONDS.sleep(50);
                 emitter.send("{\"delta\":\" " + LocalDateTime.now() + "\",\"status\":\"delta\"}");
-                TimeUnit.MILLISECONDS.sleep(250);
+                TimeUnit.MILLISECONDS.sleep(50);
                 emitter.send("{\"response\":\"hello world" + LocalDateTime.now() + "\",\"status\":\"finish\"}");
-                TimeUnit.MILLISECONDS.sleep(250);
+                TimeUnit.MILLISECONDS.sleep(50);
                 emitter.complete();
             } catch (Exception e) {
                 emitter.completeWithError(e);
@@ -42,18 +50,53 @@ public class ChatController {
     }
 
     @PostMapping("/chat")
-    public ChatResponse chat(@RequestBody ChatRequest request) {
-        log.error("{}", request);
+    public ChatResponse chat(@RequestBody ChatRequest request) throws InterruptedException {
+        log.debug("{}", request);
+
+        TimeUnit.MILLISECONDS.sleep(800);
 
         ChatResponse response = new ChatResponse();
-        response.setResponse("hello world " + LocalDateTime.now());
+        response.setResponse("<think></think>hello world " + LocalDateTime.now());
+//        response.setResponse("<think>你好啊!</think>hello world " + LocalDateTime.now());
         response.setStatus("finish");
         return response;
     }
-}
 
-@Data
-class ChatResponse {
-    private String status;
-    private String response;
+
+    @Data
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ChatRequest {
+        private String chatId;
+        private String reqId;
+        private String user;
+        private String query;
+        private Settings settings;
+        private List<Message> messages;
+
+        @Data
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        public static class Settings {
+            private String model;
+            private String systemInstruction;
+            @JsonProperty("isStreaming")
+            private boolean isStreaming;
+            private Float temperature;
+            private Float topP;
+            private Integer topK;
+            private Float repetitionPenalty;
+        }
+
+        @Data
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        public static class Message {
+            private String role;
+            private String content;
+        }
+    }
+
+    @Data
+    public static class ChatResponse {
+        private String status;
+        private String response;
+    }
 }
